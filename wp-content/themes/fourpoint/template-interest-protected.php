73fbf724741cb1e15d875e3874a82db0eb7185b4
josephsap <joesap1@gmail.com>
2016-05-06 10:30:55 -0600

    login styling back in business

--- wp-content/themes/fourpoint/template-interest-protected.php
+++ wp-content/themes/fourpoint/template-interest-protected.php
@@ -25,7 +25,7 @@ get_header();
     <?php the_field('not_logged_in_content'); ?>
     <div class="login-modal interest-owners-login open">
       <aside class="card login-card">
-<!--         <a href="#" class="close-modal"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"/></svg></a> -->
+        <a href="#" class="close-modal"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"/></svg></a>
         <div class="card-bottom">
           <div class="inner">
             <p><?php the_field('login_popup_content') ?></p>
