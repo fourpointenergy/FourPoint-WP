7d67fe1e995502623de229c7b827a07fef02451c
josephsap <joesap1@gmail.com>
2017-08-31 15:29:41 -0600

    added Google tag manager

--- wp-content/themes/fourpoint/header.php
+++ wp-content/themes/fourpoint/header.php
@@ -25,10 +25,27 @@ global $theme;
 	  ga('send', 'pageview');
 	
 	</script>
+
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-M24R3TG');</script>
+	<!-- End Google Tag Manager -->
+
 	
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
+
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M24R3TG"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
+	
 	<div class="secondary_search_wrap">
 		<form role="search" method="get" id="secondary-searchform" class="searchform search-container" action="/">
 		  <input id="secondary-search-box" type="text" class="secondary-search-box search-box" name="s" />
